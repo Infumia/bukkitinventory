75c73ebae26b5a46c596d5c8cf65a3cbf44f57b0
Hasan Demirtaş <utsukushihito@outlook.com>
2022-05-11 12:14:32 +0300

    files.

---
+++ buildSrc/src/main/kotlin/repositories.kt
@@ -0,0 +1,23 @@
+const val SNAPSHOTS = "https://oss.sonatype.org/content/repositories/snapshots/"
+
+const val SPONGEPOWERED = "https://repo.spongepowered.org/maven/"
+
+const val JCENTER = "https://jcenter.bintray.com/"
+
+const val JITPACK = "https://jitpack.io/"
+
+const val PAPERMC = "https://papermc.io/repo/repository/maven-public/"
+
+const val CODEMC = "https://repo.codemc.org/repository/maven-public/"
+
+const val CODEMC_NMS = "https://repo.codemc.org/repository/nms/"
+
+const val GLAREMASTERS = "https://repo.glaremasters.me/repository/concuncan/"
+
+const val OKAERI = "https://storehouse.okaeri.eu/repository/maven-public/"
+
+const val KARUSLABS = "https://repo.karuslabs.com/repository/chimera-releases/"
+
+const val ENGINEHUB = "https://maven.enginehub.org/repo/"
+
+const val CITIZENS = "https://repo.citizensnpcs.co/"
